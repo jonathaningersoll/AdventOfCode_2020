c51349c2c8cccb24189eb35cd4f6b340e88e3827
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-12-28 22:28:19 -0500

    Day 04 pt 2 complete

--- Day 03/Day 04/DayFour.cs
+++ Day 03/Day 04/DayFour.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Day_04
 {
@@ -31,21 +32,40 @@ namespace Day_04
             {
                 if (input[i].Length < 1)
                 {
-                    var passportProperties = new List<string>();
+
+                    var passport = new Dictionary<string, string>();
+
                     foreach (string item in fields)
                     {
-                        passportProperties.Add(item.Split(':')[0]);
+                        passport.Add(item.Split(':')[0], item.Split(':')[1]);
                     }
 
-                    if (IsValidPassport(passportProperties))
+                    if (ValidPassportFieldsAndContent(passport))
                     {
                         Console.WriteLine("Valid passport");
                         validPassports++;
                     }
                     else
                     {
-                        Console.WriteLine("Invalid passport!");
+                        Console.WriteLine("Invalid passport");
                     }
+
+                    // Part One:
+                    //var passportProperties = new List<string>();
+                    //foreach (string item in fields)
+                    //{
+                    //    passportProperties.Add(item.Split(':')[0]);
+                    //}
+
+                    //if (IsValidPassport(passportProperties))
+                    //{
+                    //    Console.WriteLine("Valid passport");
+                    //    validPassports++;
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("Invalid passport!");
+                    //}
 
                     // dump list
                     fields.Clear();
@@ -64,35 +84,184 @@ namespace Day_04
                 "****************");
         }
 
-        public bool IsValidPassport(List<string> passport)
+        //pass the fields list to ValidPassportContent since it is a potential valid passport.
+        public bool ValidPassportFieldsAndContent(Dictionary<string, string> passport)
+        {
+
+            bool validContent;
+
+            // First, validate it.
+            if (IsValidPassport(passport))
+            {
+                bool byr = false;
+                bool iyr = false;
+                bool eyr = false;
+                bool hgt = false;
+                bool hcl = false;
+                bool ecl = false;
+                bool pid = false;
+
+                // Then, for each item in passport,
+                foreach (KeyValuePair<string, string> item in passport)
+                {
+                    switch (item.Key)
+                    {
+                        case "byr":
+                            {
+                                byr = ValidateByr(Int32.Parse(item.Value));
+                                
+                                break;
+                            }
+                        case "iyr":
+                            {
+                                iyr = ValidateIyr(Int32.Parse(item.Value));
+                                
+                                break;
+                            }
+                        case "eyr":
+                            {
+                                eyr = ValidateEyr(Int32.Parse(item.Value));
+                                
+                                break;
+                            }
+                        case "hgt":
+                            {
+                                hgt = ValidateHgt(item.Value);
+                                
+                                break;
+                            }
+                        case "hcl":
+                            {
+                                hcl = ValidateHcl(item.Value);
+                                
+                                break;
+                            }
+                        case "ecl":
+                            {
+                                ecl = ValidateEcl(item.Value);
+
+                                break;
+                            }
+                        case "pid":
+                            {
+                                pid = ValidatePid(item.Value);
+
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+                }
+
+                if (byr && iyr && eyr && eyr && hgt && hcl && ecl && pid)
+                {
+                    validContent = true;
+                }
+                else
+                {
+                    validContent = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid passport");
+                validContent = false;
+            }
+
+            return validContent;
+        }
+
+        public bool IsValidPassport(Dictionary<string, string> passport)
         {
             if (
-                passport.Contains("byr")
-                && passport.Contains("iyr")
-                && passport.Contains("eyr")
-                && passport.Contains("hgt")
-                && passport.Contains("hcl")
-                && passport.Contains("ecl")
-                && passport.Contains("pid")
+                passport.ContainsKey("byr")
+                && passport.ContainsKey("iyr")
+                && passport.ContainsKey("eyr")
+                && passport.ContainsKey("hgt")
+                && passport.ContainsKey("hcl")
+                && passport.ContainsKey("ecl")
+                && passport.ContainsKey("pid")
                 )
             {
                 return true;
             }
             return false;
         }
+        public bool ValidateByr(int byr)
+        {
+            if (byr >= 1920 && byr <= 2002)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool ValidateIyr(int iyr)
+        {
+            if (iyr >= 2010 && iyr <= 2020)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool ValidateEyr(int eyr)
+        {
+            if (eyr >= 2020 && eyr <= 2030)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool ValidateHgt(string hgt)
+        {
+            // New regex:
+            Regex hgtInRg = new Regex(@"\b([6][0-9]|[5][9]|[7][0-6])[i][n]$");
+            Match hgtInMatch = hgtInRg.Match(hgt);
 
+            Regex hgtCmRg = new Regex(@"\b[1]([5-8][0-9]|[9][0-3])[c][m]$");
+            Match hgtCmMatch = hgtCmRg.Match(hgt);
+
+            if (hgtInMatch.Success || hgtCmMatch.Success) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidateHcl(string hcl)
+        {
+            Regex hclRg = new Regex(@"^[#]([a-f]|[0-9]){6}\b");
+            Match hclMatch = hclRg.Match(hcl);
+
+            if (hclMatch.Success)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool ValidateEcl(string ecl)
+        {
+            Regex eclRg = new Regex(@"^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth");
+            Match eclMatch = eclRg.Match(ecl);
+
+            if (eclMatch.Success)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public bool ValidatePid(string pid)
+        {
+            Regex pidRg = new Regex(@"\b[0-9]{9}\b");
+            Match pidMatch = pidRg.Match(pid);
+
+            if (pidMatch.Success)
+            {
+                return true;
+            }
+            else { return false; }
+        }
     }
-
-    public class Passport
-    {
-        int byr;
-        int iyr;
-        int eyr;
-        string hgt;
-        string hcl;
-        string ecl;
-        int pid;
-
-    }
-
 }
