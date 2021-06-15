f04c374a0593360b91adf0dc1b6102423cb1bd9c
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2021-06-15 10:22:56 -0400

    Added passportprocessingservice to day 4

---
+++ Day 03/Day 04/PassportProcessingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_04
+{
+    public class PassportProcessingService
+    {
+        public int ValidPassports()
+        {
+            return 0;
+        }
+    }
+}
